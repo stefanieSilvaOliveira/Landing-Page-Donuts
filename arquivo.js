fd5b3dec85e6097655f8e1955617185fd89a50e1
bonecahStefanie <stefaniesilvadeoliveira@yahoo.com.br>
2023-12-18 15:51:05 -0300

    Landing-Page-Donuts

---
+++ arquivo.js
@@ -0,0 +1,23 @@
+const titleNew = document.querySelector('#title')
+const bordBox = document.querySelector('.box')
+
+titleNew.addEventListener('mouseover', () =>{
+    titleNew.innerHTML= "Choose Donuts!"
+})
+
+titleNew.addEventListener('mouseout', () =>{
+    titleNew.innerHTML = "Abs are Cool"
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
